046cac8abcb15752fabce4329d1015ac3cfa25cb
Jeonggyu Lee <glaze@whyi.net>
2014-12-05 12:54:36 -0800

    adding sail.js

---
+++ api/models/Mesh.js
@@ -0,0 +1,20 @@
+/**
+* Mesh.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name : { type: 'string' },
+
+    size : { type: 'string' },
+
+    format : { type: 'string' },
+
+    filename : { type: 'string' }
+  }
+};
+
